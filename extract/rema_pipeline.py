301209226211c869b2b4b383f1a5a13b05e2babe
Boris Garcevic <borisgarcevic@gmail.com>
2025-08-24 12:59:04 +0200

    feat: update rema pipeline and configuration for duckdb integration

--- extract/rema_pipeline.py
+++ extract/rema_pipeline.py
@@ -1,4 +1,5 @@
 import dlt
+import os
 from dlt.sources.rest_api import RESTAPIConfig, rest_api_resources
 
 
@@ -6,7 +7,7 @@ from dlt.sources.rest_api import RESTAPIConfig, rest_api_resources
 def rema_source():
     config: RESTAPIConfig = {
         "client": {
-            "base_url": "hhttps://api.digital.rema1000.dk/api/v1/",
+            "base_url": "https://api.digital.rema1000.dk/api/v1/",
         },
         "resources": [
             {
@@ -22,11 +23,17 @@ def rema_source():
 
 
 def load_rema() -> None:
+    data_folder_name = "data"
+    os.makedirs(data_folder_name, exist_ok=True)
+
     pipeline = dlt.pipeline(
         pipeline_name="rest_api_github",
-        destination=dlt.destinations.duckdb(destination_name="rema"),
-        dataset_name="rest_api_data",
+        destination=dlt.destinations.duckdb(destination_name=f"{data_folder_name}/rema"),
+        dataset_name="rest_api_data_rema",
     )
 
     load_info = pipeline.run(rema_source())
     print(load_info)
+
+if __name__ == "__main__":
+    load_rema()
